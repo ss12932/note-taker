f1df302609e3b6f703d57a11a7c363a0404e8a74
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-07 14:07:03 +0100

    added mw, installed morgan logger mw

--- server.js
+++ server.js
@@ -3,10 +3,17 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
+
+//added PORT
+const PORT = process.env.PORT || 3001;
 
 //create new express instance
 const app = express();
 
-//added PORT
-
-const PORT = process.env.PORT || 3001;
+//middlewares
+//logger mw
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
